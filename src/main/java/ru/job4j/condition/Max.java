dfc670bdc11d9069344bd77492da4e79a1083edb
niklnn <nicklnn@ya.ru>
2022-05-01 23:05:53 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -5,8 +5,21 @@ public class Max {
         return first > second ? first : second;
     }
 
+    public static int maxTree(int first, int second, int third) {
+        return max(first, max(second, third));
+    }
+
+    public int maxFour(int first, int second, int third, int fourth) {
+        return max(first, maxTree(second, third, fourth));
+    }
+
     public static void main(String[] args) {
         int rsl = Max.max(12, 23);
         System.out.println("Максимальное число: " + rsl);
+        int rsl3 = maxTree(7, 3, 11);
+        System.out.println("Максимальное число: " + rsl3);
+        Max max = new Max();
+        int rsl4 = max.maxFour(35, 15, 22, 22);
+        System.out.println("Максимальное число: " + rsl4);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -30,4 +30,19 @@ public class MaxTest {
         int expected = 2;
         Assert.assertEquals(expected, result);
     }
+
+    @Test
+    public void whenMax1To2To3Then3() {
+        int out = Max.maxTree(1, 2, 3);
+        int expected = 3;
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenMax2To2To3To4Then4() {
+        Max max = new Max();
+        int out = max.maxFour(2, 2, 3, 4);
+        int expected = 4;
+        Assert.assertEquals(expected, out);
+    }
 }
